ea3cb9785692e921517fb752d2f14b1986f7c012
seungju1212 <seungju1212@daum.net>
2024-08-16 09:59:18 +0900

    commit

--- SS/src/main/resources/static/assets/js/navermap.js
+++ SS/src/main/resources/static/assets/js/navermap.js
@@ -35,49 +35,46 @@ let htmlMarker1 = {
 		size: N.Size(40, 40),
 		anchor: N.Point(20, 20)
 	};
-	
+
 let markerClustering = new MarkerClustering({
-				minClusterSize: 2,
-				maxZoom: 13,
-				map: null,
-				markers: [],
-				disableClickZoom: false,
-				gridSize: 120,
-				icons: [htmlMarker1, htmlMarker2, htmlMarker3, htmlMarker4, htmlMarker5],
-				indexGenerator: [10, 100, 200, 500, 1000],
-				stylingFunction: function(clusterMarker, count) {
-					$(clusterMarker.getElement()).find('div:first-child').text(count);
-				}
-			});
+	minClusterSize: 2,
+	maxZoom: 13,
+	map: null,
+	markers: [],
+	disableClickZoom: false,
+	gridSize: 120,
+	icons: [htmlMarker1, htmlMarker2, htmlMarker3, htmlMarker4, htmlMarker5],
+	indexGenerator: [10, 100, 200, 500, 1000],
+	stylingFunction: function(clusterMarker, count) {
+		$(clusterMarker.getElement()).find('div:first-child').text(count);
+	}
+});
 
 $("#all").on("click", function() {
 	remove();
 	getData(1);
-	console.log(markerClustering)
 })
 
 $("#major").on("click", function() {
 	remove();
 	getData(2);
-	console.log(markerClustering)
 })
 
 $("#tag").on("click", function() {
 	remove();
 	getData(3);
-	console.log(markerClustering)
 });
 
 function remove() {
 	markerClustering.onRemove();
-		for (let i = 0; i < markers.length; i++) {
+	for (let i = 0; i < markers.length; i++) {
 		markers[i].setMap(null);
 		markers[i].setIcon(null);
 	}
 	markers = [];
 	markerClustering.setMarkers([]);
-	console.log(markers)
 }
+
 
 function getData(type) {
 	$.ajax({
@@ -85,9 +82,11 @@ function getData(type) {
 		url: "data",
 		data: { type },
 		success: function(res) {
+			map.setOptions('zoom', 11);
+			map.setOptions('center',new naver.maps.LatLng(34.95057, 127.4874))
 			$("#list").html("");
 			for (let i = 0, ii = res.length; i < ii; i++) {
-				
+
 				let spot = res[i],
 					latlng = new naver.maps.LatLng(spot.lat, spot.lon),
 					marker = new naver.maps.Marker({
@@ -100,11 +99,11 @@ function getData(type) {
 						style="width: 100%; height: 100px; display: flex; flex-direction: row;">
 						<div>
 							<img style="width: 100px; height: 100px;"
-								src="assets/images/관광지/${spot.name }.jpg">
+								src="assets/images/관광지/${spot.name}.jpg">
 						</div>
 						<div style="width: 100%; text-align: center">
-							<h2>${spot.name }</h2>
-							<h3>${spot.address }</h3>
+							<h2>${spot.name}</h2>
+							<h3>${spot.address}</h3>
 						</div>
 					</li>`
 				$("#list").append(list);
@@ -120,18 +119,13 @@ function getData(type) {
 				});
 				infowindows.push(infowindow)
 			}
+			
 
 
 			for (let i = 0; i < markers.length; i++) {
 				naver.maps.Event.addListener(markers[i], "click", function() {
 					clickedmarker = markers[i]
-					if (selectedmarker == clickedmarker) {
-						infowindows[i].close();
-						selectedmarker.setIcon(null)
-						map.setOptions('zoom', 11)
-						selectedmarker = null
-						clickedmarker = null
-					} else if (selectedmarker != clickedmarker) {
+					if (selectedmarker != clickedmarker) {
 						if (selectedmarker == null) {
 							selectedmarker = clickedmarker
 						}
@@ -158,7 +152,7 @@ function getData(type) {
 					}
 				});
 			}
-			 markerClustering = new MarkerClustering({
+			markerClustering = new MarkerClustering({
 				minClusterSize: 2,
 				maxZoom: 13,
 				map: map,
@@ -171,8 +165,57 @@ function getData(type) {
 					$(clusterMarker.getElement()).find('div:first-child').text(count);
 				}
 			});
-			console.log(markerClustering)
+			$('ul#list li').click(function() {
+				let box = $(this).children("div")[1];
+				let title = $(box).children("h2")[0];
+				let text = $(title).text()
+				for (let i = 0; i < markers.length; i++) {
+					if (text == markers[i].title) {
+						clickedmarker = markers[i]
+						if (selectedmarker != clickedmarker) {
+							if (selectedmarker == null) {
+								selectedmarker = clickedmarker
+							}
+							selectedmarker.setIcon(null)
+							infowindows[i].close();
+							selectedmarker = clickedmarker
+							infowindows[i].open(map, clickedmarker);
+							selectedmarker.setIcon({
+
+								content: [
+									`<div style="display: flex; flex-direction: column; align-items: center; width: 50px; height: 50px;">`,
+									` <div style="display: flex; justify-content: center; align-items: center; width: 50px; height: 50px;">`,
+									` <img src="assets/images/관광지/${clickedmarker.title}.jpg" style="width: 50px; background-color: white; height: 50px; border-radius: 50%;"/>`,
+									` </div>`,
+									`</div>`
+								].join(''),
+								// url: 'resources/img/marker.png', //아이콘 경로 
+								size: new naver.maps.Size(50, 50),
+								scaledSize: new naver.maps.Size(50, 50),
+								origin: new naver.maps.Point(0, 0),
+							})
+							map.setOptions('zoom', 15)
+							map.panTo(markers[i].position);
+						}
+					}
+				}
+
+			});
+
 			
+			naver.maps.Event.addListener(map, 'zoom_changed', function(zoom) {
+				for(let i = 0; i<infowindows.length; i++){
+				if (zoom<13) {
+					if(infowindows[i].getMap()){
+					selectedmarker.setIcon(null)
+					infowindows[i].close();
+					selectedmarker = null
+					clickedmarker = null
+					}
+				}
+				}
+			});
+
 		},
 		error: function() {
 			console.log("통신에러");
