bfebbf4b6cc203bb2b919cda3cf03277deb4c0bc
seungju1212 <seungju1212@daum.net>
2024-09-02 17:39:57 +0900

    commit

--- SS/src/main/java/com/smhrd/controller/NormalController.java
+++ SS/src/main/java/com/smhrd/controller/NormalController.java
@@ -36,16 +36,17 @@ public class NormalController {
 	public String goQrPage() {
 		return "QrPage";
 	}
-	@RequestMapping("/audio1")
-	public String audio1() {
-		return "audio1";
-	}
-	
+
 	@RequestMapping("/Story")
 	public String StoryPage(Model model,int idx) {
 		List<tb_tour_site> list = repo.findById(idx);
 		model.addAttribute("list", list.get(0));
 		return "StoryPage";
+	}
+	
+	@RequestMapping("/audio1")
+	public String audio1() {
+		return "audio1";
 	}
 
 }
