c69d10768f26bb400740c3593df1843319a1f57d
seungju1212 <seungju1212@daum.net>
2024-09-03 16:37:14 +0900

    commit

--- SS/src/main/java/com/smhrd/repository/NormalRepository.java
+++ SS/src/main/java/com/smhrd/repository/NormalRepository.java
@@ -15,12 +15,6 @@ public interface NormalRepository extends JpaRepository<tb_tour_site, Integer> {
 	public List<tb_tour_site> findById(int trip_id);
 	public List<tb_tour_site> findByToursiteIn(List<String> name);
 	public List<tb_tour_site> findByTourtagLike(String name);
-	@Query("""
-			select t.tourdesc
-			from tb_tour_site t
-			where t.touridx= :trip_id
-			""")
-	public String getdetail(int trip_id);
 	public List<tb_tour_site> findByToursiteLike(String name);
 	
 }
